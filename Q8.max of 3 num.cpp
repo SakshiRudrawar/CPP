ab6759f1f4c8c09347a896c0a92712184f1ee9cf
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 20:11:12 +0530

    Add files via upload

---
+++ Q8.max of 3 num.cpp
@@ -0,0 +1,53 @@
+
+
+#include<stdio.h>
+void main()
+{
+	int a=10 , b=20, c=30;
+	
+	if(a>b && a>c)
+	 {
+	 	printf(" %d a is a max number",a);
+	 }
+	 
+	if(b>a && b>c)
+	{
+		printf(" %d b is a max number",b);
+	}
+	
+		if(c>a && c>b)
+	{
+		printf(" %d c is a max number",c);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
