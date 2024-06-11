7018ade0f70e3bbfc4d13d432fca2138e58a1d17
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 22:22:40 +0530

    Add files via upload

---
+++ Q6.perfect no using while.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i=1, num, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    while(i<=num/2)
+    {
+    	if(num % i == 0)
+    	{
+    		sum +=i;
+		}
+		i++;
+	}
+
+    if (sum == num) 
+	{
+        printf("%d is a perfect number.\n", num);
+    }
+	 else 
+	{
+        printf("%d is not a perfect number.\n", num);
+    }
+    return 0;
+
+}
+
